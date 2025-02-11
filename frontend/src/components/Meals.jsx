e6182d0705e58800da37c19dbd84e6671835da4b
harshil <159923605+HarshilBuha119@users.noreply.github.com>
2025-02-11 12:36:24 +0530

    Update Meals.jsx

--- frontend/src/components/Meals.jsx
+++ frontend/src/components/Meals.jsx
@@ -6,7 +6,7 @@ export default function Meals() {
 
   useEffect(() => {
     async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
+      const response = await fetch("https://food-ordering-app-r7sm.onrender.com");
       const meals = await response.json();
       setLoadedMeals(meals);
     }
