aeeb287a33ace9c0f5b2851d8e31a892a5968605
harshil <159923605+HarshilBuha119@users.noreply.github.com>
2025-02-20 11:51:35 +0530

    Update MealItem.jsx

--- frontend/src/components/MealItem.jsx
+++ frontend/src/components/MealItem.jsx
@@ -18,7 +18,7 @@ export default function MealItem({ meal }) {
   return (
     <li className="meal-item">
       <article>
-        <img src={`https://food-ordering-app-r7sm.onrender.com/${meal.image}`} alt={meal.name} />
+        <img src={`https://food-ordering-app-1-z69v.onrender.com/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
           <p className="meal-item-price">{currencyFormator.format(meal.price)}</p>
